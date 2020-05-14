4c9d39627810beeaef2b05ed0b4cec1b418fffbd
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-14 00:08:16 -0700

    Change gulpfile with cb function

--- Flexbox/08_autoprefixer/gulpfile.js
+++ Flexbox/08_autoprefixer/gulpfile.js
@@ -1,12 +1,14 @@
 const { task, src, dest, watch } = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 
-task("styles", function () {
+task("styles", function(done) {
   src("css/styles.css")
     .pipe(autoprefixer())
     .pipe(dest("build"));
+  done();
 });
 
-task("watch", function () {
+task("watch", function(done) {
   watch("css/styles.css", ["styles"]);
+  done();
 });
